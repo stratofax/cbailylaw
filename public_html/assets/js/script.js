d53d67089914b2ae7872758bde1a87dbf2e9e590
ratulkhan74 <ratul.khan503040@gmail.com>
2023-07-03 17:48:49 +0600

    fixed service card icon images

--- public_html/assets/js/script.js
+++ public_html/assets/js/script.js
@@ -1,10 +1,10 @@
 $(document).ready(function () {
 
-    // $(".cbailylaw__see-more").on('click', function (e) {
-    //     e.preventDefault();
-    //     $(this).prev("span").slideDown();
-    //     $(this).hide();
-    // })
+    $(".cbailylaw__see-more").on('click', function (e) {
+        e.preventDefault();
+        $(this).prev("span").slideDown();
+        $(this).hide();
+    })
 
     // Mobile menu toggler
     $toggler = $('.cbailylaw__mobile-menu__toggler');
