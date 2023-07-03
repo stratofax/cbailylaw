41c8fa6d5b81eb078f9e54633416f9d58a497633
ratulkhan74 <ratul.khan503040@gmail.com>
2023-07-03 14:25:47 +0600

    resmonsive complete for four sections

---
+++ public_html/assets/js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+
+    // $(".cbailylaw__see-more").on('click', function (e) {
+    //     e.preventDefault();
+    //     $(this).prev("span").slideDown();
+    //     $(this).hide();
+    // })
+
+    // Mobile menu toggler
+    $toggler = $('.cbailylaw__mobile-menu__toggler');
+    $toggler.on('click', function () {
+        $('nav.cbailylaw__menu').toggle();
+    });
+
+
+
+});
